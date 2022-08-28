3a76635048238a0dd5317e831005d66ad76df692
ruhulfbr <ruhul11bd@gmail.com>
2022-08-28 23:02:26 +0600

    9. Palindrome Number

---
+++ Leetcode/9. Palindrome Number/Solution.php
@@ -0,0 +1,31 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+    function isPalindrome($n) {
+        $number = $n;  
+        $sum = 0;  
+        while(floor($number)) {  
+            $rem = $number % 10;  
+            $sum = $sum * 10 + $rem;  
+            $number = $number/10;  
+        }
+        
+        if( $sum == $n ){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
+
+
+$solution = new Solution();
+
+var_dump($solution->isPalindrome(121));
+
