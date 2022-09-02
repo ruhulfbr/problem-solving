0501c2a009dc1a72ab29cf440b26e204b72ffb60
ruhulfbr <ruhul11bd@gmail.com>
2022-09-02 23:10:29 +0600

    69. Sqrt(x)

---
+++ Leetcode/69. Sqrt(x)/Solution.php
@@ -0,0 +1,12 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Integer
+     */
+    function mySqrt($x) {
+        $x = sqrt($x);
+        return intval($x);
+    }
+}
