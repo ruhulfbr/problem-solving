a6c9fed224b3561d87ac8a57898c00c4233ff629
ruhulfbr <ruhul11bd@gmail.com>
2022-09-03 23:52:59 +0600

    70. Climbing Stairs

---
+++ Leetcode/70. Climbing Stairs/Solution.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Solution {
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function climbStairs($n): int {
+
+        if ($n == 2)
+            return 2;
+        else if ($n == 1)
+            return 1;
+        $a = 0;
+        $b = 1;
+        $c = 0;
+        for ($i = 0; $i < $n; $i++)
+        {
+            $c = $a + $b;
+            $a = $b;
+            $b = $c;
+        }
+        return $c;
+
+    }
+}
+
+$solution =  new Solution();
+echo $solution->climbStairs(50000);
+
+
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo 'This is the first program';
