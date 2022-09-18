8b8a4a8e9fe83e0e5654151f0744974276e68fc4
ruhulfbr <ruhul11bd@gmail.com>
2022-09-18 10:08:14 +0600

    175. Combine Two Tables

---
+++ Leetcode/175. Combine Two Tables/solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select Person.firstName, Person.lastName, Address.city, Address.state from Person left join Address on Address.personId = Person.personId order by Person.personId asc
