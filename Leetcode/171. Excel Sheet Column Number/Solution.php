c760b9ef23804f18776f5ce0ba53d2c8f340a7b1
ruhulfbr <ruhul11bd@gmail.com>
2022-10-11 00:10:43 +0600

    171. Excel Sheet Column Number

---
+++ Leetcode/171. Excel Sheet Column Number/Solution.php
@@ -0,0 +1,25 @@
+<?php  
+
+class Solution {
+
+    /**
+     * @param String $columnTitle
+     * @return Integer
+     */
+    function titleToNumber($columnTitle) {
+        $alphabets = array_flip(range('A', 'Z'));
+		$number = 0;
+		
+		for ($i=0; $i < strlen($columnTitle); $i++) { 
+			$strPos = $alphabets[$columnTitle[$i]] + 1;
+
+			if( $i > 0 ){
+				$number = ($number * 26) + $strPos;
+			}else{
+				$number = $strPos;
+			}
+		}
+
+		return $number;
+    }
+}
