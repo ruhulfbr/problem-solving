54995065d152b16102b8f4f9758cb1b4f2c28357
ruhulfbr <ruhul11bd@gmail.com>
2022-10-11 00:11:54 +0600

    169. Majority Element

---
+++ Leetcode/169. Majority Element/Solution.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function majorityElement($nums) {
+        $mejority_el = [];
+        foreach ($nums as $key => $num) {
+        	$mejority_el[$num] = !empty($mejority_el[$num]) ? $mejority_el[$num] + 1 : 1;
+        }
+        return array_flip($mejority_el)[max($mejority_el)];
+    }
+} 
