57591da449961f6389436d65f289b1f85ddcf6ad
ruhulfbr <ruhul11bd@gmail.com>
2022-10-30 22:42:35 +0600

    806. Number of Lines To Write String

---
+++ Leetcode/806. Number of Lines To Write String/Solution.php
@@ -0,0 +1,17 @@
+<?php
+class Solution {
+    function numberOfLines($widths, $S) {
+        $S = str_split($S);
+        $rows = 1;
+        $cols = 0;
+        foreach($S as $s){
+            $width = $widths[ord($s)-ord('a')];
+            $cols += $width;
+            if($cols>100){
+                $rows++;
+                $cols = $width;
+            }
+        }
+        return [$rows,$cols];
+    }
+}
