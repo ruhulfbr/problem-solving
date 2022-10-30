c71f53977fbe61040916df225869c779d8c8ffa8
ruhulfbr <ruhul11bd@gmail.com>
2022-10-30 22:52:43 +0600

    929. Unique Email Addresses

---
+++ Leetcode/929. Unique Email Addresses/Solution.js
@@ -0,0 +1,12 @@
+var numUniqueEmails = function (emails) {
+  const set = new Set();
+
+  for (const email of emails) {
+    const arr = email.split("@");
+    arr[0] = (arr[0].split("+"))[0]; 
+    arr[0] = arr[0].replace(/\./g, "");
+    set.add(arr.join("@"));
+  }
+
+  return set.size;
+};
