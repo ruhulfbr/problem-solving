ebc928af54177440a827b1b6cb23f1fad770858b
ruhulfbr <ruhul11bd@gmail.com>
2022-10-30 23:07:24 +0600

    944. Delete Columns to Make Sorted

---
+++ Leetcode/944. Delete Columns to Make Sorted/Solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minDeletionSize(self, strs):
+        m = len(strs)
+        n = len(strs[0])
+        res = 0
+        for i in range(n):
+            for j in range(1,m):
+                if strs[j-1][i]>strs[j][i]:
+                    res += 1
+                    break
+        return res    
