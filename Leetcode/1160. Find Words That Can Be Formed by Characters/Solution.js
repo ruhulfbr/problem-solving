6b35c1da9da63ea4b0a338cadaaa04aaa778d18b
ruhulfbr <ruhul11bd@gmail.com>
2022-11-01 00:29:04 +0600

    1160. Find Words That Can Be Formed by Characters

---
+++ Leetcode/1160. Find Words That Can Be Formed by Characters/Solution.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string[]} words
+ * @param {string} chars
+ * @return {number}
+ */
+var countCharacters = function(words, chars) {
+    let myMap= new Map();
+    for(let i = 0; i< chars.length; i++){
+      if(myMap.has(chars[i])){
+        myMap.set(chars[i], myMap.get(chars[i]) + 1)
+      }else{
+        myMap.set(chars[i], 1)
+      }
+    }
+
+    let totalCount = 0
+
+    words.map(word => {
+
+      let allPass = true
+      let wordMap = new Map();
+      for (let i = 0; i < word.length; i++) {
+        if(myMap.has(word[i])){
+          
+          let count = !wordMap.get(word[i]) ? 1 : wordMap.get(word[i]) + 1;
+          wordMap.set(word[i], count)
+        }else{
+          allPass = false  
+          break;
+        }
+      }
+      let lengthPass = true;
+      
+      if(allPass){
+        let countLen = 0
+        wordMap.forEach((value, index) => {
+            countLen += value;
+            if(value > myMap.get(index)){
+                lengthPass = false
+            }   
+        })
+
+        if( lengthPass){
+            totalCount += countLen;
+        }
+      }
+     
+    })
+    return totalCount;
+};
