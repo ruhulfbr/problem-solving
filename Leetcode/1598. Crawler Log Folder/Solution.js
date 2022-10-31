9941c75c382bd437bc0c1f145c61bded1e941a3d
ruhulfbr <ruhul11bd@gmail.com>
2022-11-01 00:38:25 +0600

    1598. Crawler Log Folder

---
+++ Leetcode/1598. Crawler Log Folder/Solution.js
@@ -0,0 +1,17 @@
+var minOperations = function(logs) {
+    let curIdx = 0;
+
+    for(let i = 0; i < logs.length; i++) {
+      if(logs[i] === "../") {
+          curIdx <= 0 ? curIdx : curIdx -= 1;
+      } else if(logs[i] === "./") {
+          curIdx;
+      } else if(logs[i] === "x/") {
+          curIdx += 1;
+      } else {
+          curIdx += 1;
+      } 
+    }
+    
+    return curIdx;
+};
