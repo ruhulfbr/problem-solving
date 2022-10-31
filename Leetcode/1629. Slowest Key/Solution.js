f52550c1ad2b55933060351cb0f52521e5791b03
ruhulfbr <ruhul11bd@gmail.com>
2022-11-01 00:41:18 +0600

    1629. Slowest Key

---
+++ Leetcode/1629. Slowest Key/Solution.js
@@ -0,0 +1,18 @@
+var slowestKey = function(releaseTimes, keysPressed) {
+    releaseTimes.unshift(0)
+    let str = "", min = 0, max = 0;
+    for (let i = 1; i < releaseTimes.length; i++) {
+        max = releaseTimes[i] - releaseTimes[i - 1];
+        if (max > min) {
+            str = keysPressed[i - 1]
+        } else if (max === min) {
+            if (str < keysPressed[i - 1]) {
+                str = keysPressed[i - 1];
+            }
+        }
+        if (max > min) {
+            min = max;
+        }
+    }
+    return str;
+};
