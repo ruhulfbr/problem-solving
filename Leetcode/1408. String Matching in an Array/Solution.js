980873d56b5097980e09380e62b5a06eaf830b3e
ruhulfbr <ruhul11bd@gmail.com>
2022-11-01 00:31:48 +0600

    1408. String Matching in an Array

---
+++ Leetcode/1408. String Matching in an Array/Solution.js
@@ -0,0 +1,22 @@
+var stringMatching = function(words) {
+    let matchingArr = [];
+    let min = 0;
+    for (let i = 1; i < words.length; i++) {
+        if (words[min].includes(words[i]) ) {
+            matchingArr.push(words[i]);
+            words.splice(i,1);
+            i = 0;
+        } else if(words[i].includes(words[min]) ) {
+            matchingArr.push(words[min]);
+            words.splice(min,1);
+            i = 0; 
+        }
+        else {
+             if (i === words.length-1) {
+                words.splice(min,1)
+                i = 0;
+            }
+        }
+    }
+    return matchingArr;
+};
