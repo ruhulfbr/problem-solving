1c56cc5613fe90bafa0ceb7901f52835873f6a13
ruhulfbr <ruhul11bd@gmail.com>
2022-11-01 23:18:45 +0600

    1773. Count Items Matching a Rule

---
+++ Leetcode/1773. Count Items Matching a Rule/Solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+     let res = 0;
+    for(let i of items){
+      if(ruleKey=="color" && i[1]==ruleValue){
+        res++
+      }else if(ruleKey=="type" && i[0]==ruleValue){
+        res++
+      }else if(ruleKey=="name" && i[2]==ruleValue){
+        res++
+      }
+    }
+    return res;
+};
