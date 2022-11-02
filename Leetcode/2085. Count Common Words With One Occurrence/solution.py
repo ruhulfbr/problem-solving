823aaa629928a9c8a5d550a521441fc57e7867a4
ruhulfbr <ruhul11bd@gmail.com>
2022-11-03 00:38:06 +0600

    2085. Count Common Words With One Occurrenc

---
+++ Leetcode/2085. Count Common Words With One Occurrence/solution.py
@@ -0,0 +1,12 @@
+#2085. Count Common Words With One Occurrence
+
+class Solution:
+  def countWords(self, words1: List[str], words2: List[str]) -> int:
+    count = Counter(words1)
+
+    for word in words2:
+      if word in count and count[word] < 2:
+        count[word] -= 1
+
+    return sum(value == 0 for value in count.values())
+
