cd539e6646c12e6c0cd72b1e431008405db07b15
Md. Ruhul Amin <ruhul11bd@gmail.com>
2022-11-25 14:51:33 +0600

    22. Generate Parentheses

---
+++ Leetcode/22. Generate Parentheses/Solution.js
@@ -0,0 +1,25 @@
+//22. Generate Parentheses
+
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+    const result = [];
+    generate(result, "", 0, 0, n);
+    return result;
+};
+
+function generate(result, s, open, close, n) {
+    if (open === n && close === n) {
+        result.push(s);
+        return;
+    }
+    if (open < n) {
+        generate(result, s + "(", open + 1, close, n);
+    }
+    if (close < open) {
+        generate(result, s + ")", open, close + 1, n);
+    }
+};
